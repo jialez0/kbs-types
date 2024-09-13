6a0122d90d8dd8131492f0a98bbd8a46f2a356b1
Jiale Zhang <zhangjiale@linux.alibaba.com>
2024-09-13 15:53:09 +0800

    Tee Type: add system as new type
    Signed-off-by: Jiale Zhang <zhangjiale@linux.alibaba.com>

--- src/lib.rs
+++ src/lib.rs
@@ -39,6 +39,9 @@ pub enum Tee {
     // This value is only used for testing an attestation server, and should not
     // be used in an actual attestation scenario.
     Sample,
+
+    // System Attestation
+    System,
 }
 
 #[derive(Clone, Serialize, Deserialize, Debug)]
